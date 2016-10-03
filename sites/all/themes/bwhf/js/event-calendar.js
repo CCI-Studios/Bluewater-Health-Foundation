42dc174da2581154206c5d13b9097429741cca90
Chris Morris <cmorris@ccistudios.com>
2016-10-03 15:54:26 -0400

    show multiday events correctly in the event calendar close #13

--- sites/all/themes/bwhf/js/event-calendar.js
+++ sites/all/themes/bwhf/js/event-calendar.js
@@ -12,19 +12,20 @@
     var events = [];
     $container.find(".views-row").each(function(){
       var title = $(this).find(".views-field-title").text().trim();
-      var date = $(this).find(".views-field-field-event-date").text().trim();
+      var start = $(this).find(".views-field-field-event-date .date-display-start").text().trim();
+      var end = $(this).find(".views-field-field-event-date .date-display-end").text().trim();
       var url = $(this).find(".views-field-path").text().trim();
-      if (date) {
+      if (start) {
         events.push({
           title: title,
-          start: date,
+          start: start,
+          end: end,
           url: url,
         });
       }
     });
     fullCalendar = $container.fullCalendar({
       events: events,
-      allDayDefault: true,
       header: {
         left: 'prev,next',
         center: '',
@@ -35,13 +36,20 @@
       },
       displayEventTime: false,
       eventRender: function (event, element, view) {
-        if (event.start.month() !== view.intervalStart.month() || event.start.year() !== view.intervalStart.year()) {
-          return false;
+        var dateStart = event.start.format('YYYY-MM-DD');
+        view.el.find('[data-date="' + dateStart + '"]:not(.fc-other-month)').addClass('fc-has-event');
+        if (event.end) {
+          var diff = event.end.diff(event.start, "days");
+          for (var i=0; i<diff; i++) {
+            var newDate = moment(event.start.valueOf()).add(i+1, "days");
+            view.el.find('[data-date="' + newDate.format('YYYY-MM-DD') + '"]:not(.fc-other-month)').addClass('fc-has-event');
+          }
         }
-        var dateString = moment(event.start).format('YYYY-MM-DD');
-        view.el.find('[data-date="' + dateString + '"]:not(.fc-other-month)').addClass('fc-has-event');
         if (event.url) {
           element.href = event.url;
+        }
+        if (event.start.month() !== view.intervalStart.month() || event.start.year() !== view.intervalStart.year()) {
+          return false;
         }
       },
     });
