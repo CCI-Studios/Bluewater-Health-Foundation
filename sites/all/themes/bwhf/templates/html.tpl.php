14b60aa7fb2cef6b572ce0eb7f40e405692430b5
Chris Morris <cmorris@ccistudios.com>
2018-10-17 14:02:04 -0400

    added google tag manager code

--- sites/all/themes/bwhf/templates/html.tpl.php
+++ sites/all/themes/bwhf/templates/html.tpl.php
@@ -48,10 +48,23 @@
   <?php print $head; ?>
   <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
   <title><?php print $head_title; ?></title>
+
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-T2JZS9N');</script>
+  <!-- End Google Tag Manager -->
+
   <?php print $styles; ?>
   <?php print $scripts; ?>
 </head>
 <body class="<?php print $classes; ?>" <?php print $attributes;?>>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T2JZS9N"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
   <div id="skip-link">
     <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
   </div>
